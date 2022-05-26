b4b030133b2295fb72fbdae415e4e066a9bdfbc0
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-27 00:30:44 +0500

    for/in and for/of

--- index.js
+++ index.js
@@ -1,44 +1,84 @@
 console.log('hello')
 // if
 
-let x=y=2;
-let z = 3;
+// let x=y=2;
+// let z = 3;
 
-if(x===y){
-    if(z===3){
-        console.log('z = 3') // true
-    }else{
-        console.log("z != 3") // false
-    }
-    console.log('x = y') // true
-}else{
-    console.log('x != y') //false
+// while(x<=10){
+//     console.log(`x = ${x}`)
+//     x++
+// }
+// if(x===y){
+//     if(z===3){
+//         console.log('z = 3') // true
+//     }else{
+//         console.log("z != 3") // false
+//     }
+//     console.log('x = y') // true
+// }else{
+//     console.log('x != y') //false
+// }
+
+// if(x===1){
+//     console.log('x = 1')
+// }else if(x===2){
+//     console.log('x = 2')
+// }else{
+//     console.log('error')
+// }
+
+// switch(x){
+//     case 1:{
+//         console.log('x == 1')
+//         break
+//     };
+//     case 3:{
+//         console.log('x == 3')
+//         break
+//     }
+//     case 2:{
+//         console.log('x == 2')
+//         break
+//     }
+//     default:{
+//         break
+//     }
+// }
+
+// let i = 0
+// let j = 10     
+// function sum(i,j){ 
+//     let summ = 0
+//     for(i = 0, j = 2; j<10; i++, j++){
+//         sum = i + j
+//     }
+//     console.log(`i + j = ${sum}`) 
+// }
+
+// sum(i,j)
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let sum = 0 
+for(let element of data){
+    sum += element
 }
 
-if(x===1){
-    console.log('x = 1')
-}else if(x===2){
-    console.log('x = 2')
-}else{
-    console.log('error')
+console.log(sum)
+let element = 0
+while(element<10){
+    element++
+    console.log(element)
 }
 
-switch(x){
-    case 1:{
-        console.log('x == 1')
-        break
-    };
-    case 3:{
-        console.log('x == 3')
-        break
-    }
-    case 2:{
-        console.log('x == 2')
-        break
-    }
-    default:{
-        break
-    }
+let obj = {
+    name: 'Humoyun',
+    age: 19
 }
+for(b in obj){
+    console.log(obj[b])
+    obj.age+=1
+}
+
+
 
 
